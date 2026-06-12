395b90b4508aa32570c54dab2eaa3f196631d3ac
Amay Jha <83952941+AMAYKJHA@users.noreply.github.com>
2026-06-12 18:10:04 +0545

    feat: Add serializers to chat app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     
+    "rest_framework",
+    "corsheaders",
+    "pgvector",
+    
     # Project apps
     "src.chat.apps.ChatConfig",
     "src.game.apps.GameConfig",
