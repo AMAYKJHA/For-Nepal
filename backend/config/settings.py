91026eca546317dd80456581e4fdb58725e516ea
Amay Jha <83952941+AMAYKJHA@users.noreply.github.com>
2026-06-12 17:57:06 +0545

    update: Minor updates in the backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    # Project apps
+    "src.chat.apps.ChatConfig",
+    "src.game.apps.GameConfig",
 ]
 
 MIDDLEWARE = [
--- backend/src/chat/admin.py
+++ backend/src/chat/admin.py
@@ -1,3 +1,7 @@
+# chat/admin.py
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Memory
+
+admin.site.register(Memory)
