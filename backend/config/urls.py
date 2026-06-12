9eaf48ea25c31a123c8b2d117282a87875cebbd5
Amay Jha <83952941+AMAYKJHA@users.noreply.github.com>
2026-06-12 20:06:43 +0545

    feat: Redirect to api docs when on homepage

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.shortcuts import redirect
+
 from chat import urls as chat_urls
 from accounts import urls as auth_urls
 
+
+def homepage(request):
+    return redirect("swagger-ui")
+
 urlpatterns = [
+    path("", homepage, name="home-page"),
     path("admin/", admin.site.urls),
     path("api/auth/", include(auth_urls)),
     path("api/chat/", include(chat_urls)),
